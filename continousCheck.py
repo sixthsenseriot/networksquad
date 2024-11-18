5ea5dc9182fbc1813fdadbab20e142f8fb8e9476
Adam Nguyen <ahnguyen12300@gmail.com>
2024-11-18 13:58:47 -0600

    Adding terminal commands

--- continousCheck.py
+++ continousCheck.py
@@ -1,4 +1,5 @@
 import platform
+import subprocess
 
 # Get the system name (e.g., 'Windows', 'Darwin' for macOS, 'Linux')
 os_name = platform.system()
@@ -7,6 +8,14 @@ print("Operating System:", os_name)
 
 if os_name == "Darwin":
     print("Running on macOS")
+    print("this is where the test is gonna go")
+    '''
+    try:
+        result = subprocess.run(["bash", "/path/to/script.sh"], check=True, capture_output=True, text=True)
+        print("Script Output:", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e.stderr}")
+    '''
 elif os_name == "Windows":
     print("Running on Windows")
 elif os_name == "Linux":
